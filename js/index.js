4a65180a9a0a3bfad203a139ce51135ab47ad2c8
Braulio Gonzalez <jarvis@Braulios-MacBook-Air.local>
2018-03-15 21:39:57 -0700

    add transition to nav bar

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
+// scroll function to id section of page
 $(function() {
-  $('.js-nav a, .js-connect').click(function(e) {
+  $(".js-nav a, .js-connect").click(function(e) {
     e.preventDefault();
-    $('body, html').animate({
-      scrollTop: $($.attr(this, 'href')).offset().top
-    }, 750);
+    $("body, html").animate(
+      {
+        scrollTop: $($.attr(this, "href")).offset().top
+      },
+      750
+    );
   });
 });
+
+//bootstrap scrolly
+$("body").scrollspy({ target: "#navbar-example" });
